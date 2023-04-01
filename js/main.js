6aeefc8ccb3227636289b10f8a0ef88770727586
Omar Ashraf <124902211+Omar-Abosea3@users.noreply.github.com>
2023-04-01 15:20:06 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -56,9 +56,9 @@ function ClearForm(){
 function displayAllWebsites(){
     var cartoona="";
     for(var i=0;i<allWebsites.length;i++){
-         cartoona+=`<tr class='d-flex justify-content-between align-items-center px-2'>
+         cartoona+=`<tr class='d-flex justify-content-between align-items-center px-5'>
         <td class="TdFont fw-bold fs-5">${allWebsites[i].bookMarkName}</td>
-        <td><a href="${allWebsites[i].bookMarkLinks}" target="_blank" class="link-light TdFont btn btn-primary me-2 text-decoration-none">Visit</a>
+        <td><span class="pe-2"><a href="${allWebsites[i].bookMarkLinks}" target="_blank" class="link-light TdFont btn btn-primary text-decoration-none">Visit</a></span>
         <button class="btn btn-danger TdFont" onclick="DelBookMark(${i})">Delete</button>
         </td>
     </tr>`
